e3b9c57e61a55fe81c7c8d12341b8097b755a219
Rafael Greif <rafaelgreif@freenet.de>
2022-01-22 19:09:32 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,114 @@
+'use strict';
+console.clear();
+    
+    let = sentence(hello + bye);
+
+    let = hello;
+
+function hello() {
+    let inhalt;
+    inhalt = 'Hello';
+}
+
+const hello = 'Hello';
+
+    let = bye;
+
+function bye() {
+    let inhalt; 
+    inhalt = 'Bye';
+}
+
+const bye = 'Bye';
+
+        if (hello) {
+            console.log('Ich bin ein neuer Bot.');
+        }
+        else {
+            console.log('Hallo, wie geht es dir?');
+        }
+
+    let age = 1;
+    let days = 1 * 365;
+
+console.log('Mein Alter ist' + age + 'Jahr');
+
+    let = rechenAufgabe;
+    let x = 1;
+
+function rechenAufgabe(x) {
+    return x * 1 * 12;
+}
+
+const rechenAufgabe = x;
+
+console.log('Ich habe soeben mein Alter ausgerechnet (1 Jahr = 12 Monate).');
+
+    let input = prompt('Bitte Quadratmeterzahl eingeben!');
+    let result = meterToHektar(y);
+    let y = 10000;
+
+// Bot stellt frage, Leser auf Seite soll sie durch Input beantworten
+
+console.log('Kannst du diese Aufgabe berechnen: Wie viel Quadratmeter sind ein Hektar?');
+
+function meterToHektar() {
+    return 1 * 100000;
+}
+
+        if (input === 10000) {
+            console.log('Diese Antwort ist richtig.');
+        }
+        else {
+            console.log('Diese Antwort ist leider falsch. Multipliziere einfach die "ein Hektar" mit 10000. Dann ist die Antwort "10000 Quadratmeter"');
+        }
+
+    let answer = console.log('Diese Antwort ist richtig.');
+
+const answer;    
+
+        if (answer) {
+            console.log('Das hat ja bis jetzt gut geklappt.');
+            console.log('Dann werde ich dir noch eine Frage stellen.');
+        }
+        else {
+            console.log('Okay, dann musst du dir noch mal die Umformung beim Flächeninhalt anschauen.');
+            console.log('Dann werde ich dir noch eine Frage stellen.');
+        }
+
+// Bot stellt zweite Frage, Antwort wird wieder erwartet        
+
+    let input = prompt('Bitte Alter angeben!');
+
+function frageVolljährig() {
+    console.log('Wie alt bist du?')
+
+        if (input < 18) {
+        console.log('Du bist nicht volljährig.');
+        }
+
+        if (input >= 18) {
+            console.log('Du bist volljährig.');
+        }
+    }
+
+console.log('Ich kann bis 100 zählen.');
+
+        for (let i = 0; i < 101; i++) {
+            console.log(i);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
